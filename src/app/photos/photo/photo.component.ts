16f89d9fc5fc9e62ee21a8cbefad8dabacc62fbb
Pedro <pedro@Aspire-A315-42-5b28b50a.localdomain>
2021-09-12 23:45:16 -0300

    feature: alurapic

---
+++ src/app/photos/photo/photo.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-photo',
+  templateUrl: './photo.component.html',
+  styleUrls: ['./photo.component.scss']
+})
+export class PhotoComponent implements OnInit {
+
+  @Input() url = '';
+
+  @Input() description='';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
