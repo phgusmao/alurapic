22222eb8a104695589a390ede1363fa06519216b
phgusmao <pedro.gusmao1998@hotmail.com>
2021-09-15 08:15:56 -0300

    feature: alterações

---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-photo',
+  templateUrl: 'photo.component.html'
+})
+
+export class PhotoComponent {
+
+  description?: 'Leão';
+  url?: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbery_Lion.jpg/440px-Sultan'
+
+}
